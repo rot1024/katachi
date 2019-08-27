7f918ba41b3818b1d713758b16df9be67260b341
rot1024 <aayhrot@gmail.com>
2019-08-28 04:01:23 +0900

    add import alias

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.tsx?$/,
@@ -11,6 +13,10 @@ module.exports = ({ config }) => {
       "react-docgen-typescript-loader"
     ]
   });
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    "@katachi": path.resolve(__dirname, "../src")
+  };
   config.resolve.extensions.push(".ts", ".tsx");
   return config;
 };
--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,12 @@
+const path = require("path");
+
 module.exports = {
   webpack: {
-    alias: {
-      "react-dom": "@hot-loader/react-dom"
+    resolve: {
+      alias: {
+        "react-dom": "@hot-loader/react-dom",
+        "@katachi": path.resolve(__dirname, "./src")
+      }
     }
   },
   plugins: [{ plugin: require("craco-plugin-react-hot-reload") }]
