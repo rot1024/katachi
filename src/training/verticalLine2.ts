a0d3e0ef5e57751416067b6eb75bc7449680a9dd
rot1024 <aayhrot@gmail.com>
2019-08-29 02:14:17 +0900

    tweak training lib

--- src/training/verticalLine2.ts
+++ src/training/verticalLine2.ts
@@ -3,9 +3,10 @@ import { TrainingMenu } from "./common";
 const verticalLine2: TrainingMenu = {
   paramsSize: 3,
   stateSize: 1,
-  validateParams: params => true,
+  validateParams: ([barLength, bar2Length, ratio]) =>
+    Math.abs(barLength - bar2Length) >= 0.1 && ratio > 0.1 && ratio < 0.9,
   judgeScore: (params, state) =>
-    Math.max(100, Math.abs(params[2] - state[1])) / 100
+    1 - Math.min(0.3, Math.abs(params[2] - state[1])) / 0.3
 };
 
 export default verticalLine2;
