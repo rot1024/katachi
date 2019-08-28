b9943057f0dea2c5073d6a102a4480118c61dfc2
rot1024 <aayhrot@gmail.com>
2019-08-29 01:44:43 +0900

    add story

--- src/components/core/Training/index.stories.tsx
+++ src/components/core/Training/index.stories.tsx
@@ -4,11 +4,19 @@ import { storiesOf } from "@storybook/react";
 
 import Component, { TrainingType } from ".";
 
-storiesOf("Training", module).add("Vertical Line", () => (
-  <Component
-    type={TrainingType.VerticalLine2}
-    params={[0.5, 0.5, 0.5]}
-    screenSize={500}
-    isAnswerShown
-  />
-));
+storiesOf("Training", module)
+  .add("Vertical Line", () => (
+    <Component
+      type={TrainingType.VerticalLine2}
+      params={[0.5, 0.5, 0.5]}
+      screenSize={500}
+    />
+  ))
+  .add("Vertical Line with answer", () => (
+    <Component
+      type={TrainingType.VerticalLine2}
+      params={[0.5, 0.5, 0.5]}
+      screenSize={500}
+      isAnswerShown
+    />
+  ));
