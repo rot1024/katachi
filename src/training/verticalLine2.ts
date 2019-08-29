5ac14cfb5ebe3b56c7e6a0d8bb023ab93af2f3cf
rot1024 <aayhrot@gmail.com>
2019-08-30 01:26:21 +0900

    tweak verticalLine2 params

--- src/training/verticalLine2.ts
+++ src/training/verticalLine2.ts
@@ -4,8 +4,8 @@ const verticalLine2: TrainingMenu = {
   paramsSize: 3,
   stateSize: 1,
   validateParams: ([barLength, bar2Length, ratio]) =>
-    barLength > 0.2 &&
-    bar2Length > 0.2 &&
+    barLength > 0.3 &&
+    bar2Length > 0.3 &&
     Math.abs(barLength - bar2Length) >= 0.2 &&
     ratio > 0.1 &&
     ratio < 0.9,
