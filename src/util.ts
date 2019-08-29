17980bb6c69687bb40cda2048c0f5d643603aa35
rot1024 <aayhrot@gmail.com>
2019-08-29 23:44:44 +0900

    add numberArrayEqual to util

--- src/util.ts
+++ src/util.ts
@@ -1,2 +1,5 @@
 export const clamp = (v: number, min: number, max: number) =>
   v > max ? max : v < min ? min : v;
+
+export const numberArrayEqual = (a: number[], b: number[]) =>
+  a.length === b.length && a.every((c, i) => c === b[i]);
