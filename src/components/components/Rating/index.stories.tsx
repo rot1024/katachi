a568fc2e97541f65d922cac66e640cb783130c59
rot1024 <aayhrot@gmail.com>
2019-08-30 02:47:11 +0900

    add Rating component

---
+++ src/components/components/Rating/index.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import Component from ".";
+
+storiesOf("Rating", module)
+  .add("default", () => (
+    <div>
+      <Component score={1} />
+      <Component score={0.9} />
+      <Component score={0.8} />
+      <Component score={0.7} />
+      <Component score={0.6} />
+    </div>
+  ))
+  .add("small", () => (
+    <div>
+      <Component small score={1} />
+      <Component small score={0.9} />
+      <Component small score={0.8} />
+      <Component small score={0.7} />
+      <Component small score={0.6} />
+    </div>
+  ));
---
+++ src/components/components/Rating/index.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+import { getRating, Rating as RatingEnum } from "@katachi/training";
+
+export interface Props {
+  className?: string;
+  score: number;
+  small?: boolean;
+}
+
+const styles: { [key in RatingEnum]: { color: string } } = {
+  [RatingEnum.Legend]: { color: "red" },
+  [RatingEnum.Perfect]: { color: "red" },
+  [RatingEnum.Excellent]: { color: "red" },
+  [RatingEnum.Good]: { color: "red" },
+  [RatingEnum.Bad]: { color: "red" }
+};
+
+const Rating: React.FC<Props> = ({ className, score, small }) => {
+  const rating = getRating(score);
+  return (
+    <StyledSvg
+      rating={rating}
+      className={className}
+      version="1.1"
+      width={small ? 100 : 170}
+      height={small ? 30 : 50}
+      viewBox={small ? "0 0 100 30" : "0 0 170 50"}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <defs>
+        <filter id="inset-shadow">
+          <feOffset dx="2" dy="3" />
+          <feGaussianBlur stdDeviation="1" result="offset-blur" />
+          <feComposite
+            operator="out"
+            in="SourceGraphic"
+            in2="offset-blur"
+            result="inverse"
+          />
+          <feFlood flood-color="black" flood-opacity="0.40" result="color" />
+          <feComposite operator="in" in="color" in2="inverse" result="shadow" />
+          <feComposite operator="over" in="shadow" in2="SourceGraphic" />
+        </filter>
+      </defs>
+      <text
+        x="50%"
+        y="50%"
+        text-anchor="middle"
+        dominant-baseline="central"
+        fontSize={small ? 16 : 24}
+        fontWeight="bold"
+        fill={styles[rating].color}
+        stroke="#000"
+        strokeWidth={1}
+        filter="url(#inset-shadow)"
+      >
+        {rating.toUpperCase()}
+      </text>
+    </StyledSvg>
+  );
+};
+
+const StyledSvg = styled.svg<{ rating: RatingEnum }>`
+  display: inline-block;
+`;
+
+export default Rating;
