13722ba82f6d78e457591f5c63fdef26a2db24a4
rot1024 <aayhrot@gmail.com>
2019-08-30 03:05:48 +0900

    add rating display to TrainingResult

--- src/components/components/TrainingResult/index.tsx
+++ src/components/components/TrainingResult/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { TrainingType } from "@katachi/training";
 
+import Rating from "../Rating";
+
 export { TrainingType };
 
 export interface Props {
@@ -9,14 +11,24 @@ export interface Props {
   scores: number[];
 }
 
+const humanReadableScore = (score: number) => Math.round(score * 100);
+
 const TrainingResult: React.FC<Props> = ({ className, scores, type }) => {
+  const totalScore =
+    scores.length > 0 ? scores.reduce((a, b) => a + b, 0) / scores.length : 0;
   return (
     <div className={className}>
       <h1>{type}</h1>
+      <p>
+        Total: {humanReadableScore(totalScore)} <Rating score={totalScore} />
+      </p>
       <ul>
         {scores.map((s, i) => (
           <li>
-            {i}: {s}
+            <span>
+              {i + 1}: {humanReadableScore(s)}
+            </span>
+            <Rating small score={s} />
           </li>
         ))}{" "}
       </ul>
