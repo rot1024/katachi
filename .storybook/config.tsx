5d86e147c6abefbd2c286d008e4b0a31a2ca7c30
rot1024 <aayhrot@gmail.com>
2019-08-31 02:15:18 +0900

    make global styling with emotion

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,6 +1,9 @@
+import React from "react";
 import { configure, addDecorator, addParameters } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
+import { ThemeProvider } from "emotion-theming";
 import { themes } from "@storybook/theming";
+import { GlobalStyle, theme as appTheme } from "../src/style";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../src", true, /\.stories\.tsx$/);
@@ -10,6 +13,13 @@ addParameters({
     theme: themes.normal
   }
 });
+
+addDecorator(cb => (
+  <ThemeProvider theme={appTheme}>
+    <GlobalStyle />
+    {cb()}
+  </ThemeProvider>
+));
 
 function loadStories() {
   addDecorator(withInfo);
---
+++ src/style.tsx
@@ -0,0 +1,70 @@
+import React, { FC } from "react";
+import { Global, css } from "@emotion/core";
+import emotionStyled, { CreateStyled } from "@emotion/styled";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faShapes,
+  faChartLine,
+  faSlidersH
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faShapes, faChartLine, faSlidersH);
+
+export const theme = {};
+
+export const styled = emotionStyled as CreateStyled<typeof theme>;
+
+export const GlobalStyle: FC = () => (
+  <Global
+    styles={css`
+      body {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+          "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+          "Helvetica Neue", sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+
+      code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+          monospace;
+      }
+
+      .App {
+        text-align: center;
+      }
+
+      .App-logo {
+        animation: App-logo-spin infinite 20s linear;
+        height: 40vmin;
+        pointer-events: none;
+      }
+
+      .App-header {
+        background-color: #282c34;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: calc(10px + 2vmin);
+        color: white;
+      }
+
+      .App-link {
+        color: #61dafb;
+      }
+
+      @keyframes App-logo-spin {
+        from {
+          transform: rotate(0deg);
+        }
+        to {
+          transform: rotate(360deg);
+        }
+      }
+    `}
+  />
+);
