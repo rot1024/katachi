4546b6bd63ac7e2715bd438ec0df747e9c586a80
rot1024 <aayhrot@gmail.com>
2019-08-31 03:07:36 +0900

    add nav component

---
+++ src/components/components/Nav/index.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import Component, { Mode } from ".";
+
+storiesOf("Nav", module)
+  .add("default", () => <Component onSelect={action("select")} />)
+  .add("training selected", () => (
+    <Component onSelect={action("select")} selected={Mode.Training} />
+  ))
+  .add("report selected", () => (
+    <Component onSelect={action("select")} selected={Mode.Report} />
+  ))
+  .add("config selected", () => (
+    <Component onSelect={action("select")} selected={Mode.Config} />
+  ));
---
+++ src/components/components/Nav/index.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { styled } from "@katachi/style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export enum Mode {
+  Training,
+  Report,
+  Config
+}
+
+export interface Props {
+  className?: string;
+  selected?: Mode;
+  onSelect?: (mode: Mode) => void;
+}
+
+const Nav: React.FC<Props> = ({ className, selected, onSelect }) => {
+  return (
+    <Wrapper className={className}>
+      <MenuItem
+        active={selected === Mode.Training}
+        onClick={() => onSelect && onSelect(Mode.Training)}
+      >
+        <div>
+          <Icon icon="shapes" />
+          トレーニング
+        </div>
+      </MenuItem>
+      <MenuItem
+        active={selected === Mode.Report}
+        onClick={() => onSelect && onSelect(Mode.Report)}
+      >
+        <div>
+          <Icon icon="chart-line" />
+          レポート
+        </div>
+      </MenuItem>
+      <MenuItem
+        active={selected === Mode.Config}
+        onClick={() => onSelect && onSelect(Mode.Config)}
+      >
+        <div>
+          <Icon icon="sliders-h" />
+          設定
+        </div>
+      </MenuItem>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: stretch;
+  height: 5em;
+`;
+
+const MenuItem = styled.div<{ active?: boolean }>`
+  flex: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  text-align: center;
+  font-size: 0.8rem;
+  font-weight: bold;
+  color: ${({ active, theme }) => (active ? theme.accent : theme.inactive)};
+`;
+
+const Icon = styled(FontAwesomeIcon)`
+  display: block;
+  margin: 0 auto 0.2em;
+  font-size: 1.5rem;
+`;
+
+export default Nav;
