9497dad9bd1e0da806a4523de75f773c88711838
rot1024 <aayhrot@gmail.com>
2019-09-01 02:29:35 +0900

    add ReportPage

---
+++ src/components/pages/ReportPage/index.stories.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import Component from ".";
+
+storiesOf("ReportPage", module).add("default", () => <Component />);
---
+++ src/components/pages/ReportPage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface Props {
+  className?: string;
+}
+
+const ReportPage: React.FC<Props> = ({ className }) => {
+  return <div className={className}>WIP</div>;
+};
+
+export default ReportPage;
