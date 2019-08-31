050b434a083fbf9a449b1b310bfa5f539ea4c3f6
rot1024 <aayhrot@gmail.com>
2019-09-01 03:05:28 +0900

    implement simple app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 /** @jsx jsx */
-import React from "react";
+import React, { useState } from "react";
 import { hot } from "react-hot-loader/root";
 import { css, jsx } from "@emotion/core";
 
 import { GlobalStyle, ThemeProvider } from "./style";
 import AppContainer, { Mode } from "./components/components/AppContainer";
+import HomePage from "./components/pages/HomePage";
+import TrainingMenuPage, { Level } from "./components/pages/TrainingMenuPage";
 import TrainingPage, { TrainingType } from "./components/pages/TrainingPage";
+import ReportPage from "./components/pages/ReportPage";
+import SettingPage, { Item } from "./components/pages/SettingPage";
+import ScaleCorrectionPage from "./components/pages/ScaleCorrectionPage";
+
+enum Route {
+  Home,
+  TrainingMenu,
+  Training,
+  Report,
+  Setting,
+  ScaleCorrection
+}
 
 const App: React.FC = () => {
+  const [currentRoute, jumpTo] = useState<Route>(Route.Home);
+  const [scaleCorrection, setScaleCorrection] = useState(1);
+  const [training, setTraining] = useState<[TrainingType, Level]>();
+
   return (
     <ThemeProvider>
       <GlobalStyle />
@@ -16,14 +34,81 @@ const App: React.FC = () => {
           width: 100%;
           height: 100%;
         `}
-        currentMode={Mode.Training}
-        onModeChange={(...args) => console.log("mode changed", ...args)}
+        currentMode={
+          currentRoute === Route.Training || currentRoute === Route.TrainingMenu
+            ? Mode.Training
+            : currentRoute === Route.Report
+            ? Mode.Report
+            : currentRoute === Route.Setting
+            ? Mode.Setting
+            : undefined
+        }
+        navHidden={
+          currentRoute === Route.Home || currentRoute === Route.Training
+        }
+        onModeChange={mode => {
+          if (mode === Mode.Training) {
+            jumpTo(Route.TrainingMenu);
+          }
+          if (mode === Mode.Report) {
+            jumpTo(Route.Report);
+          }
+          if (mode === Mode.Setting) {
+            jumpTo(Route.Setting);
+          }
+        }}
+        headerClosable={currentRoute === Route.Training}
+        onHeaderClose={() => {
+          jumpTo(Route.TrainingMenu);
+          setTraining(undefined);
+        }}
       >
-        <TrainingPage
-          type={TrainingType.VerticalLine2}
-          duration={10000}
-          onFinish={(...args) => console.log("training finished", ...args)}
-        />
+        {currentRoute === Route.Home && (
+          <HomePage
+            onSignIn={() => {
+              jumpTo(Route.TrainingMenu);
+            }}
+          />
+        )}
+        {currentRoute === Route.TrainingMenu && (
+          <TrainingMenuPage
+            onSelect={(type, level) => {
+              setTraining([type, level]);
+              jumpTo(Route.Training);
+            }}
+          />
+        )}
+        {currentRoute === Route.Training && training && (
+          <TrainingPage
+            type={training[0]}
+            level={training[1]}
+            scaleCorrection={scaleCorrection}
+            onFinish={() => {
+              jumpTo(Route.TrainingMenu);
+            }}
+          />
+        )}
+        {currentRoute === Route.Report && <ReportPage />}
+        {currentRoute === Route.Setting && (
+          <SettingPage
+            onSelect={mode => {
+              if (mode === Item.ScaleCorrection) {
+                jumpTo(Route.ScaleCorrection);
+              }
+              // if (mode === Item.SignOut) {
+              //   jumpTo(Route.Home);
+              // }
+            }}
+          />
+        )}
+        {currentRoute === Route.ScaleCorrection && (
+          <ScaleCorrectionPage
+            onScaleCorrectionEnter={sc => {
+              setScaleCorrection(sc);
+              jumpTo(Route.Setting);
+            }}
+          />
+        )}
       </AppContainer>
     </ThemeProvider>
   );
