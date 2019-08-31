da6ec236445c5d35fd828fa52493cd60f7d96ca0
rot1024 <aayhrot@gmail.com>
2019-08-31 16:13:08 +0900

    display simple app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
+/** @jsx jsx */
 import React from "react";
 import { hot } from "react-hot-loader/root";
+import { css, jsx } from "@emotion/core";
 
 import { GlobalStyle, ThemeProvider } from "./style";
+import AppContainer, { Mode } from "./components/components/AppContainer";
+import TrainingPage, { TrainingType } from "./components/pages/TrainingPage";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider>
       <GlobalStyle />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppContainer
+        css={css`
+          width: 100%;
+          height: 100%;
+        `}
+        currentMode={Mode.Training}
+        onModeChange={(...args) => console.log("mode changed", ...args)}
+      >
+        <TrainingPage
+          type={TrainingType.VerticalLine2}
+          duration={10000}
+          onFinish={(...args) => console.log("training finished", ...args)}
+        />
+      </AppContainer>
     </ThemeProvider>
   );
 };
