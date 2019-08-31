d02a6fc2a95f2dfd97daf550aabe014d23bfae0b
rot1024 <aayhrot@gmail.com>
2019-09-01 02:33:55 +0900

    add HomePage

---
+++ src/components/pages/HomePage/index.tsx
@@ -0,0 +1,26 @@
+/** @jsx jsx */
+import React from "react";
+import { css, jsx } from "@emotion/core";
+
+import Button from "@katachi/components/components/Button";
+
+export interface Props {
+  className?: string;
+  onSignIn?: () => void;
+}
+
+const HomePage: React.FC<Props> = ({ className, onSignIn }) => {
+  return (
+    <div
+      className={className}
+      css={css`
+        padding: 1em;
+      `}
+    >
+      <h1>形を正確に観る "眼" を養うトレーニング</h1>
+      <Button onClick={onSignIn}>Twitterでログイン</Button>
+    </div>
+  );
+};
+
+export default HomePage;
