ff77e019f05419b77e75543f6416ca76c0e136f6
rot1024 <aayhrot@gmail.com>
2019-08-31 16:11:37 +0900

    fix craco config

--- craco.config.js
+++ craco.config.js
@@ -2,11 +2,9 @@ const path = require("path");
 
 module.exports = {
   webpack: {
-    resolve: {
-      alias: {
-        "react-dom": "@hot-loader/react-dom",
-        "@katachi": path.resolve(__dirname, "./src")
-      }
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+      "@katachi": path.resolve(__dirname, "./src")
     }
   },
   plugins: [{ plugin: require("craco-plugin-react-hot-reload") }]
