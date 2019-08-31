634bd8e60d5de2b5326b11dc93e98a50f47236a6
rot1024 <aayhrot@gmail.com>
2019-08-31 16:31:46 +0900

    add use-resize-observer

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+
+declare module "use-resize-observer" {
+  import { RefObject } from "react";
+  const useResizeObserver: () => [RefObject<any>, number, number];
+  export default useResizeObserver;
+}
