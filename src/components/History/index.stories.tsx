a3c8a3e1e5e1f3985eac2a9f990bd69265fd0df5
rot1024 <aayhrot@gmail.com>
2019-09-03 03:26:58 +0900

    fix

--- src/components/History/index.stories.tsx
+++ src/components/History/index.stories.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 
 import Component from ".";
-import { TrainingType } from "../Training";
+import { TrainingType, Level } from "@katachi/lib";
 
 storiesOf("History", module).add("default", () => (
   <Component
     histories={[
       {
         type: TrainingType.VerticalLine1,
+        level: Level.Normal,
         params: [[0.1, 0.5, 0.9], [0.1, 0.5, 0.9], [0.1, 0.5, 0.9]],
         scores: [0.9, 0.8, 0.7],
         datetime: new Date(),
@@ -16,6 +17,7 @@ storiesOf("History", module).add("default", () => (
       },
       {
         type: TrainingType.VerticalLine1,
+        level: Level.Ultimate,
         params: [[0.1, 0.5, 0.9], [0.1, 0.5, 0.9], [0.1, 0.5, 0.9]],
         scores: [0.9, 0.8, 0.7],
         datetime: new Date(),
